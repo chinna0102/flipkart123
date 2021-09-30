1de786c696774f6483e9ff1b67a66bda6b3009cb
chinna0102 <87141075+chinna0102@users.noreply.github.com>
2021-09-30 15:21:30 +0530

    Update App.java
    changed

--- src/main/java/com/flipkart/App.java
+++ src/main/java/com/flipkart/App.java
@@ -1,13 +1,13 @@
 package com.flipkart;
 
 /**
- * Hello cricketfanscsk!
+ * Hello cricketfans!
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello cricketfanscsk!@" );
+        System.out.println( "Hello cricketfans!@" );
     }
 }
